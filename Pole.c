1b1f8ccb26098506d24ace1c36624f005ec0e77b
Alex <alekaytokarek80@gmail.com>
2024-01-28 19:45:23 +0100

    Rewizja kolo

--- Pole.c
+++ Pole.c
@@ -3,9 +3,14 @@
 
 float a = 0;
 float b = 0;
+float r = 0;
 
 float obliczeniePolaProstokata(float a, float b){
     return (a*b);
+}
+
+float obliczeniePolaKola(float r){
+    return(M_PI*pow(r, 2));
 }
 int main() {
 	printf("Podaj pierwszy bok prostokata:\t");
@@ -13,5 +18,9 @@ int main() {
     printf("Podaj drugi bok prostokata:\t");
     scanf("%f", &b);
     printf("Pole prostokata wynosi:\t%f", obliczeniePolaProstokata(a, b));
+
+    printf("\nPodaj promien kola:\t");
+    scanf("%f", &r);
+    printf("Pole kola wynosi: \t%f", obliczeniePolaKola(r));
     return 0;
 }
